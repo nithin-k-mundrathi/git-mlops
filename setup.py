3e1f57b2a1e75c55cfc522d10765435c7163eee4
Nithin Mundrathi <nithin.k.mundrahi@gmail.com>
2025-03-24 19:00:28 +0000

    Push to Branch

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+with open('requirements.txt') as f:
+    requirements =  f.read().splitlines()
+
+setup(
+    name = 'mlops-project-2',
+    version= '0.1',
+    author = 'Nithin',
+    packages = find_packages(), # detect other packages and import them - src, config, utils.
+    install_requires = requirements,
+)
+
